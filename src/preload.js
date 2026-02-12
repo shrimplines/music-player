2365703343eb751f6b56be9a5d7bdf2df9f86d2e
Shrimp <one.walmart.bag@gmail.com>
2026-02-12 12:52:56 -0800

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,13 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  // This sends the click to the main process
   sendControl: (command) => ipcRenderer.send('media-control', command),
-  
-  // This listens for the "Sent" confirmation to flash your screen green
+
   onTerminalLog: (callback) => ipcRenderer.on('terminal-log', (event, msg) => callback(msg)),
   
-  // Metadata listeners so 'song' and 'artist' actually update
   onSpotifyData: (callback) => ipcRenderer.on('spotify-data', (event, data) => callback(data)),
   onProgressTick: (callback) => ipcRenderer.on('progress-tick', (event, percent) => callback(percent))
 });
